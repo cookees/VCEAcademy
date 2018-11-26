3df27f1ebe732f761e59f689f4a57a40d150e152
cookees <33850508+cookees@users.noreply.github.com>
2018-11-26 17:10:01 +1100

    Navigation Updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import logo from './logo.svg';
 import './App.css';
+import HeaderBar from './Components/HeaderBar/HeaderBar'
+import Banner from './Components/Banner/Banner'
+import CourseList from './Components/CourseList/CourseList'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.myRef = React.createRef();
+    this.state = {
+      current: 1
+    }
+    this.scrollToContent = this.scrollToContent.bind(this);
+    this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
+  }
+
+  handleFilterUpdate(filterValue) {
+            this.setState({
+                  current: filterValue
+            });
+            this.scrollToContent()
+      }
+
+  scrollToContent() {
+    this.myRef.current.scrollIntoView({behavior: 'smooth'});
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderBar/>
+        <Banner/>
+        <div ref={this.myRef}>
+        {this.state.current === 1 ?
+          <div>
+            <CourseList change={this.handleFilterUpdate}/>
+          </div> : null
+        }
+        {this.state.current === 2 ?
+          <div>
+            <Banner/>
+          </div> : null
+        }
+        </div>
+
       </div>
     );
   }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Banner(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <img
+      src="http://www.wallpapermaiden.com/wallpaper/21936/download/1366x768/flat-landscape-mountains-digital-art.jpg"
+      alt="new"
+      style={{
+        width: window.innerWidth,
+        height: window.innerHeight * 0.70,
+      }}
+      />
+    </div>
+  );
+}
+
+export default (Banner);
